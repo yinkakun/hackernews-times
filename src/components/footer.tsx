7225d2f4c4ecfe47813859b45c6d13190a812b1a
Yinka Adedire <yinkakun@gmail.com>
2021-12-22 12:05:28 +0100

    add dates to header and footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Container from '@components/container';
+import dayjs from 'dayjs';
 
 const Footer = () => {
   return (
-    <footer className="py-2 text-center">
-      <Container>The Hackernews Times</Container>
+    <footer className="py-2 text-center border-t border-raisin-black">
+      <Container>
+        <p className="text-sm">
+          &copy; {dayjs().format('YYYY')}, Hackernews Times.
+        </p>
+      </Container>
     </footer>
   );
 };
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Container from '@components/container';
+import dayjs from 'dayjs';
 
 const Header = () => {
+  const now = dayjs();
+
   return (
-    <header className="py-2 text-center">
-      <Container>The Hackernews Times</Container>
+    <header className="py-4 text-center border-double border-y-4 border-raisin-black">
+      <Container>
+        <h1 className="text-2xl font-heading">Hackernews Times</h1>
+        <div className="mt-2">
+          <span className="text-sm">{now.format('MMMM D, YYYY')}</span>
+        </div>
+      </Container>
     </header>
   );
 };
